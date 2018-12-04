c0518b2bfaaafdbe4ba04eb2751aa4d74281726b
Antonio Moura <antonniogt@gmail.com>
2018-12-04 21:47:44 -0200

    WIP

--- src/test/java/HU1CargaCanalTest.java
+++ src/test/java/HU1CargaCanalTest.java
@@ -1,95 +1,30 @@
-package br.senai.sc.edu.projetomaria.service;
+import static org.junit.jupiter.api.Assertions.fail;
 
+import java.net.URISyntaxException;
 import java.nio.file.Path;
-import java.util.List;
+import java.nio.file.Paths;
+import java.sql.SQLException;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.Test;
 
-import br.senai.sc.edu.projetomaria.dao.CanalDAO;
-import br.senai.sc.edu.projetomaria.dao.HistoricoDAO;
-import br.senai.sc.edu.projetomaria.dao.ProdutoDAO;
-import br.senai.sc.edu.projetomaria.model.Canal;
-import br.senai.sc.edu.projetomaria.model.Historico;
-import br.senai.sc.edu.projetomaria.model.Produto;
-import br.senai.sc.edu.projetomaria.resource.Messages;
+import br.senai.sc.edu.projetomaria.service.CargaService;
 
-public class RelatorioService {
-	private static final Logger LOGGER = LogManager.getLogger();
-
-	public ServiceResponse exportarFamilia(Path path) {
-//		FamiliaWriter.CSVWriter(path);
-		throw new UnsupportedOperationException(Messages.ERRO_METODO_NAO_IMPLEMENTADO);
-	}
-	
-	public ServiceResponse exportarFamilia() {
-		throw new UnsupportedOperationException(Messages.ERRO_METODO_NAO_IMPLEMENTADO);
-	}
-
-	public ServiceResponse exportarProduto(Path path) {
-		ProdutoDAO pdao = new ProdutoDAO();
-		List<Produto> produtos = pdao.exportarProdutos();
-		ServiceResponse response = new ServiceResponse(ServiceResponse.STATUS.OK, produtos);
+class HU1CargaCanalTest {
+ 	
+	@Test		
+	void testeInsert() throws SQLException {		
+		ClassLoader classLoader	= getClass().getClassLoader();
+		CargaService service = new CargaService();
+		Path p = null;
+		try {
+			p = Paths.get(classLoader.getResource("dataset/carga_canal.csv").toURI());
+		} catch (URISyntaxException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+			
+		}
+		service.cargaCanal(p);
+		fail("falhou");
 		
-		return response;
-	}
-	
-	public ServiceResponse exportarProduto() {
-		throw new UnsupportedOperationException(Messages.ERRO_METODO_NAO_IMPLEMENTADO);
-	}
-
-	public ServiceResponse exportarCanal(Path path) {
-//		CanalWriter cw = new CanalWriter();
-//		cw.generateRelatorio(path);
-//		String message = String.format(Messages.SUCESSO_RELATORIO_CANAL, path);
-//		LOGGER.info(message);
-		throw new UnsupportedOperationException(Messages.ERRO_METODO_NAO_IMPLEMENTADO);
-	}
-
-	public ServiceResponse exportarCanal() {
-
-		CanalDAO cdao = new CanalDAO();
-		List<Canal> canais = cdao.getCanais();
-		ServiceResponse response = new ServiceResponse(ServiceResponse.STATUS.OK, canais);
-		
-		return response;
-	}
-
-	public ServiceResponse exportarHistorico(Path path) {
-//		HistoricoWriter escritor = new HistoricoWriter();
-//		HistoricoDAO dao = new HistoricoDAO();
-//		List<Historico> registros = dao.get();
-//		escritor.writeCsvFile(path, registros);
-		throw new UnsupportedOperationException(Messages.ERRO_METODO_NAO_IMPLEMENTADO);
-	}
-
-	public ServiceResponse exportarHistorico() {
-		
-		HistoricoDAO hdao = new HistoricoDAO();
-		List<Historico> historicos = hdao.get();
-		ServiceResponse response = new ServiceResponse(ServiceResponse.STATUS.OK, historicos);
-	
-		return response;
-	}
-
-	public ServiceResponse exportarPhase(Path path) {
-//		PhaseWritter writter = new PhaseWritter();
-//		writter.gerarArquivoPhase(path);
-		throw new UnsupportedOperationException(Messages.ERRO_METODO_NAO_IMPLEMENTADO);
-	}
-	
-	public ServiceResponse exportarPhase() {
-		throw new UnsupportedOperationException(Messages.ERRO_METODO_NAO_IMPLEMENTADO);
-	}
-		
-	public ServiceResponse exportarEstimativa(Path path, int periodoAnterior) {
-//		EstimativaWritter estimativa = new EstimativaWritter();
-//		estimativa.escrever(path, periodoAnterior);	
-		throw new UnsupportedOperationException(Messages.ERRO_METODO_NAO_IMPLEMENTADO);
-	}
-	
-	public ServiceResponse exportarEstimativa(int periodoAnterior) {
-		throw new UnsupportedOperationException(Messages.ERRO_METODO_NAO_IMPLEMENTADO);
-	}	
-	
-}
+	}		
+ }	
